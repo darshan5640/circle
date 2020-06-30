0c9cfb4f4d19c6b12f752d182b28a45cc2713e20
kishanLakkad <dev016.rejoicehub@gmail.com>
2020-06-30 23:43:15 +0530

    change port

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use((req, res, next) => {
     next();
 });
 
-var port = process.env.port || 80
+var port = process.env.port || 3000
 app.listen(port, () => {
     console.log("Server running on port", port);
 });
